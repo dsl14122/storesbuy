74c2bc292b57fd0a1cab1900fea96974d56beccb
dsl14122 <1596720698@qq.com>
2019-04-30 10:12:04 +0800

    初始化init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueRouter from "vue-router"
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+
+const  routes=[
+
+]
+
+const router = new VueRouter({
+  routes // (缩写) 相当于 routes: routes
+})
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
